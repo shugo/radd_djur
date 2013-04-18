9d6f913a0ecaa677bab53f8c9421dd8cbf0d41de
Shugo Maeda <shugo@ruby-lang.org>
2013-04-18 22:21:44 +0900

    Add any_char_spec.rb.

---
+++ spec/haensel/grammar/parsers/any_char_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+describe Grammar::Parsers do
+  describe "#any_char" do
+    it "returns a Parser which parses any character" do
+      d = Grammar::Derivs.new(nil, "abc")
+      r = any_char.parse(d)
+      expect(r.succeeded?).to eq true
+      expect(r.value).to eq ?a
+      expect(r.remainder.char.value).to eq ?b
+    end
+  end
+end
