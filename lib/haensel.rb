dd27fbaa9d9c75acd276eef5ba3a560be2a0923d
Shugo Maeda <shugo@ruby-lang.org>
2013-04-18 22:58:44 +0900

    Check whether RubyVM is defined.

--- lib/haensel.rb
+++ lib/haensel.rb
@@ -1,10 +1,20 @@
-old_option = RubyVM::InstructionSequence.compile_option
-RubyVM::InstructionSequence.compile_option = {
-  trace_instruction: false,
-  tailcall_optimization: true
-}
-begin
+def with_tailcall_optimization
+  if defined?(RubyVM)
+    old_option = RubyVM::InstructionSequence.compile_option
+    RubyVM::InstructionSequence.compile_option = {
+      trace_instruction: false,
+      tailcall_optimization: true
+    }
+  end
+  begin
+    yield
+  ensure
+    if defined?(RubyVM)
+      RubyVM::InstructionSequence.compile_option = old_option
+    end
+  end
+end
+
+with_tailcall_optimization do
   require "haensel/grammar"
-ensure
-  RubyVM::InstructionSequence.compile_option = old_option
 end
